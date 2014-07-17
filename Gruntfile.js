a7ec99ebd465f48d733ff2aac0ce7c36d526c0b3
magdev <magdev3.0@gmail.com>
2014-07-17 13:52:30 +0200

    removed tests from watch-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,37 @@
 'use strict';
 
-module.exports = function (grunt) {
-  // Show elapsed time at the end
-  require('time-grunt')(grunt);
-  // Load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+module.exports = function(grunt) {
+	// Show elapsed time at the end
+	require('time-grunt')(grunt);
+	// Load all grunt tasks
+	require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib: {
-        src: ['lib/**/*.js']
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib: {
-        files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		jshint : {
+			options : {
+				jshintrc : '.jshintrc',
+				reporter : require('jshint-stylish')
+			},
+			gruntfile : {
+				src : 'Gruntfile.js'
+			},
+			lib : {
+				src : [ 'lib/**/*.js' ]
+			}
+		},
+		watch : {
+			gruntfile : {
+				files : '<%= jshint.gruntfile.src %>',
+				tasks : [ 'jshint:gruntfile' ]
+			},
+			lib : {
+				files : '<%= jshint.lib.src %>',
+				tasks : [ 'jshint:lib' ]
+			}
+		}
+	});
 
-  // Default task.
-  grunt.registerTask('default', ['jshint']);
+	// Default task.
+	grunt.registerTask('default', [ 'jshint' ]);
 };
