34801b6c574329de30b90421d27af9027ee91f85
magdev <magdev3.0@gmail.com>
2014-07-19 14:41:58 +0200

    fixed ip-check for proxy-requests, added tests

--- test/mashape-tools_test.js
+++ test/mashape-tools_test.js
@@ -10,7 +10,54 @@
 
 var request = require('supertest'), 
     express = require('express'),
-    mashape = require('../');
+    mashape = require('../'),
+    
+    allowedIps = [
+        '107.23.255.128',
+        '107.23.255.129',
+        '107.23.255.130',
+        '107.23.255.131',
+        '107.23.255.132',
+        '107.23.255.133',
+        '107.23.255.134',
+        '107.23.255.135',
+        '107.23.255.136',
+        '107.23.255.137',
+        '107.23.255.138',
+        '107.23.255.139',
+        '107.23.255.140',
+        '107.23.255.141',
+        '107.23.255.142',
+        '107.23.255.143',
+        '107.23.255.144',
+        '107.23.255.145',
+        '107.23.255.146',
+        '107.23.255.147',
+        '107.23.255.148',
+        '107.23.255.149',
+        '107.23.255.150',
+        '107.23.255.151',
+        '107.23.255.152',
+        '107.23.255.153',
+        '107.23.255.154',
+        '107.23.255.155',
+        '107.23.255.156',
+        '107.23.255.157',
+        '107.23.255.158',
+        '107.23.255.159',
+        '54.209.110.14',
+        '54.86.14.38',
+        '54.86.225.32',
+        '54.86.245.80',
+        '54.88.28.135',
+        '54.88.28.194',
+        '54.88.37.172',
+        '54.88.51.199',
+        '54.88.53.251',
+        '54.88.54.104',
+        '54.88.55.60',
+        '54.88.55.63'
+    ];
 
 
 describe('ipFilter()', function() {
@@ -47,6 +94,86 @@ describe('ipFilter()', function() {
         
         request(app)
             .get('/')
+            .expect(403, done);
+    });
+    
+    it('allow valid proxy (single ip)', function(done) {
+        var app = express();
+        app.set('trust proxy', true);
+        
+        app.use(mashape.ipFilter({
+            iplist: allowedIps,
+            strict: false,
+            log: false
+        }));
+        
+        app.get('/', function(req, res){
+            res.send(200);
+        });
+        
+        request(app)
+            .get('/')
+            .set('x-forwarded-for', '107.23.255.128')
+            .expect(200, done);
+    });
+    
+    it('deny invalid proxy (single ip)', function(done) {
+        var app = express();
+        app.set('trust proxy', true);
+        
+        app.use(mashape.ipFilter({
+            iplist: allowedIps,
+            strict: false,
+            log: false
+        }));
+        
+        app.get('/', function(req, res){
+            res.send(200);
+        });
+        
+        request(app)
+            .get('/')
+            .set('x-forwarded-for', '10.2.2.20')
+            .expect(403, done);
+    });
+    
+    it('allow valid proxy (multiple ips)', function(done) {
+        var app = express();
+        app.set('trust proxy', true);
+        
+        app.use(mashape.ipFilter({
+            iplist: allowedIps,
+            strict: false,
+            log: false
+        }));
+        
+        app.get('/', function(req, res){
+            res.send(200);
+        });
+        
+        request(app)
+            .get('/')
+            .set('x-forwarded-for', '10.2.2.20,107.23.255.128')
+            .expect(200, done);
+    });
+    
+    it('deny invalid proxy (multiple ips)', function(done) {
+        var app = express();
+        app.set('trust proxy', true);
+        
+        app.use(mashape.ipFilter({
+            iplist: allowedIps,
+            strict: false,
+            log: false
+        }));
+        
+        app.get('/', function(req, res){
+            res.send(200);
+        });
+        
+        request(app)
+            .get('/')
+            .set('x-forwarded-for', '10.2.2.20,10.2.2.22')
             .expect(403, done);
     });
 });
