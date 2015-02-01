13a087cf44c37b9a9ee7c6315aa4dcd368171954
magdev <magdev3.0@gmail.com>
2015-02-01 09:00:30 +0100

    removed autdiscovery from test

--- test/service-container_test.js
+++ test/service-container_test.js
@@ -15,7 +15,7 @@ if (MASHAPE_KEY) {
      * Test ServiceContainer
      */
     describe('ServiceContainer()', function() {
-        it('load (autodiscovery off)', function(done) {
+        it('load', function(done) {
             var app = express();
             
             app.use(mashape.serviceContainer({
@@ -38,7 +38,7 @@ if (MASHAPE_KEY) {
         });
         
         
-        it('manual register and call magdev/GermanBanks (autodiscovery off)', function(done) {
+        it('manual register and call magdev/GermanBanks', function(done) {
             var app = express();
             
             app.use(mashape.serviceContainer({
@@ -75,47 +75,14 @@ if (MASHAPE_KEY) {
                 .expect(200, done);
         });
         
-        it('load (autodiscovery on)', function(done) {
+        it('call mashape:/users/mashaper/apis/mashape', function(done) {
             var app = express();
             
             app.use(mashape.serviceContainer({
                 strict: false,
                 log: false,
                 debug: DEBUG,
-                mashapeKey: MASHAPE_KEY,
-                autodiscovery: true
-            }));
-            
-            app.get('/', function(req, res) {
-                if (!req.service) {
-                    return res.send(500);
-                }
-                
-                req.service.info('mashape', function(e, info) {
-                    if (e) {
-                        return res.send(500);
-                    }
-                    if (!info) {
-                        return res.send(404);
-                    }
-                    return res.send(200);
-                });
-            });
-            
-            request(app)
-                .get('/')
-                .expect(200, done);
-        });
-        
-        it('call mashape:/users/mashaper/apis/mashape (autodiscovery on)', function(done) {
-            var app = express();
-            
-            app.use(mashape.serviceContainer({
-                strict: false,
-                log: false,
-                debug: DEBUG,
-                mashapeKey: MASHAPE_KEY,
-                autodiscovery: true
+                mashapeKey: MASHAPE_KEY
             }));
             
             app.get('/', function(req, res) {
@@ -126,47 +93,6 @@ if (MASHAPE_KEY) {
                 req.service.call('mashape', 'get', '/users/mashaper/apis/mashape', {}, function(result) {
                     if (!result || result.code === 404) {
                         return res.send(404);
-                    }
-                    return res.send(result.code);
-                });
-            });
-            
-            request(app)
-                .get('/')
-                .expect(200, done);
-        });
-        
-        
-        it('discover and call magdev/GermanBanks (autodiscovery on)', function(done) {
-            var app = express();
-            
-            app.use(mashape.serviceContainer({
-                strict: false,
-                log: false,
-                debug: DEBUG,
-                mashapeKey: MASHAPE_KEY,
-                autodiscovery: true
-            }));
-            
-            app.use(function(req, res, next) {
-                req.service.discover('magdev/GermanBanks', function(e, container) {
-                    if (DEBUG) {
-                        console.log(container.services);
-                    }
-                    return next();
-                });
-            });
-            
-            app.get('/', function(req, res) {
-                if (!req.service) {
-                    return res.send(500);
-                }
-                req.service.call('germanbanks', 'get', '/', {}, function(result) {
-                    if (!result || result.code === 404) {
-                        return res.send(404);
-                    }
-                    if (DEBUG) {
-                        console.log(result.body);
                     }
                     return res.send(result.code);
                 });
